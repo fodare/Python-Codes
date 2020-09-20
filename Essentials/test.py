c0eb7218b4e78593c8394dc165055df735cad047
fodare <folorunsodamilare007@hotmail.com>
2020-09-20 12:01:12 +0300

    Python Lessons

---
+++ Essentials/test.py
@@ -0,0 +1,6 @@
+"""
+This is a doc string. They are used to describe what the file and what the file contains.
+They contain the meaning of the files and what the content of the file does.
+"""
+# That the Value Hello world and print it to the Terminal.
+print("Hello World")
