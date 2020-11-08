b7f589f7df0fcf1a91e3b672c28ea633d1bb0f3a
fodare <folorunsodamilare007@hotmail.com>
2020-11-08 10:37:33 +0200

    Restructuring learning materials

--- Essentials/ageCalculator.py
+++
@@ -1,6 +0,0 @@
-"""
-Simple Age Calculator.
-"""
-user_birthDate = input("Please enter your year of birth: ")
-age = 2020 - int(user_birthDate)
-print(f"Your age this year would be: {age}")
--- Essentials/inputs.py
+++
@@ -1,9 +0,0 @@
-"""
-To take inputs from the user 
-via the keyboard simply use the 
-input keyword. Lik below.
-"""
-
-print("Welcome to the Python test bed.")
-userName = input('Please enter your name: ')
-print(userName)
