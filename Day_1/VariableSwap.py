8ab654336d80c2d0e3b7cc454b2b13ce06e509bb
Folorunso <Damilare.Folorunso@arvato.com>
2021-03-01 18:37:14 +0100

    Day 1, complete string manipulation, Variables and basic debugging

---
+++ Day_1/VariableSwap.py
@@ -0,0 +1,6 @@
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+c = a
+a = b
+b = c
+print(f"a: {a}\nb: {b}")
---
+++ Day_1/main.py
@@ -0,0 +1,20 @@
+# Day 1 :
+# Understanding printing, commenting, Debugging
+# String manipulation and Variables.
+
+# Goal: Band name generator.
+
+# ToDo 1: Print a greeting to the user.
+print("Welcome to the band name generator!")
+
+# ToDo 2: Ask the user for the city they grew up in.
+city_name = input("What city did you grow up in: \n")
+
+# ToDo 3: Ask the user for their pet name.
+pet_name = input("What is your pet name: \n")
+
+# ToDo 4: Combine the name of their city and the pet name as their band name
+band_name = city_name + pet_name
+print(f"Your band name could be: {band_name}")
+
+# ToDo 5: Make sure the input cursor shows a new line
