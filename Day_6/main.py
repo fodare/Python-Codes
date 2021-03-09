7e35c9ce6bd25a2f380683eb448f5d9df07ad45c
fodare <folorunsodamilare007@hotmail.com>
2021-03-09 18:24:26 +0100

    Day 6, complete python functions, code blocks and while loops

--- Day_6/main.py
+++ Day_6/main.py
@@ -2,3 +2,14 @@
 # Fuctions, Code Blocks and While loops
 
 # Goal: Escape the Maze game
+
+game_on = True
+while game_on:
+    user_name = input("What is your name: ")
+    if user_name == "Damilare":
+        game_on = False
+        print("Access Granted!")
+    else:
+        game_on = False
+        print("Nope not correct")
+
