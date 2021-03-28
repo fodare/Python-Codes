aa3da9d1fb6e6fd0891d2e2bc45fdf908f57897e
fodare <folorunsodamilare007@hotmail.com>
2021-03-28 12:46:28 +0200

    complete Python Dictionaries, nesting method, accessing method and manipulation methods

--- Day_9/main.py
+++ Day_9/main.py
@@ -1,12 +1,36 @@
 # Day 9
-'''Dictionaries and Nesting'''
+"""Dictionaries and Nesting"""
 
 # Goal: Blind auction biding game
 
 from Logo import logo as gabbel
 
-# Todo 1: Print the gabble and welcome the user to the game
+'''Variables'''
+bidders = {}
+
+more_bidders = True
+
 print(gabbel)
-player_name = input(f"What is your name: \n")
-bid_amount = int(input(f"What's your bid ?: $ \n"))
-other_bidders = input(f"Are there any other bidders ?. Type 'yes' or 'no': \n")
+print("Welcome to the secret auction program!")
+
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]  # Get the bidder amount
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount  # Set the highest amount from the bidders list
+            winner = bidder  # The winner becomes the bidder with the  highest amount
+    print(f"The winner is {winner} with the bid amount $ {highest_bid}")
+
+
+while more_bidders:
+    bidder_name = input("What is your name: ")
+    bidder_amount = int(input("Enter you bid amount: $ "))
+    bidders[bidder_name] = bidder_amount
+    more_bid = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+    if more_bid == "yes":
+        more_bidders = True
+    else:
+        more_bidders = False
+        find_highest_bidder(bidders)
--- Day_9/test.py
+++ Day_9/test.py
@@ -22,29 +22,64 @@
 
 
 # Todo 1: Create a grading program
-student_scores = {
-    "Harry": 81,
-    "Ron": 78,
-    "Hermione": 99,
-    "Draco": 74,
-    "Neville": 62
+# student_scores = {
+#     "Harry": 81,
+#     "Ron": 78,
+#     "Hermione": 99,
+#     "Draco": 74,
+#     "Neville": 62
+# }
+#
+# student_grade = {}
+#
+#
+# def print_grade(score_list):
+#     for student in score_list:
+#         score = student_scores[student]
+#         if score > 90:
+#             student_grade[student] = "Outstanding"
+#         elif score > 80:
+#             student_grade[student] = "Excellent"
+#         elif score > 70:
+#             student_grade[student] = "Very good"
+#         else:
+#             student_grade[student] = "Failed class"
+#     print(student_grade)
+#
+#
+# print_grade(score_list=student_scores)
+
+'''Nesting Dictionary in a dictionary '''
+travel_log = {
+    "India": {"cities_visited": ["New-Delhi", "Mumbai", "UP", "Mumbai", "Bangalore"],
+              "total_visits": 1},
+    "Estonia": {"cities_visited": ["Tallinn"],
+                "total_visits": 1},
+    "Sweden": {"cities_visited": ["Stockholm", "Eskilstuna"],
+               "total_visits": 1},
+    "Germany": {"cities_visited": ["Frankfurt", "Munich", "Osnaburk", "Munster"],
+                "total_visits": 1},
 }
 
-student_grade = {}
+'''Nesting Dictionary in a List '''
 
+language_log = [
+    {
+        "name": "Java",
+        "experience": "12 Month",
+        "Sample_project": "Null",
+    },
+    {
+        "name": "Python",
+        "experience": "4 Month",
+        "Sample_project": "Yes",
+    },
+    {
+        "name": "JavaScript",
+        "experience": "4 Month",
+        "Sample_project": "Yes",
+    },
+]
 
-def print_grade(score_list):
-    for student in score_list:
-        score = student_scores[student]
-        if score > 90:
-            student_grade[student] = "Outstanding"
-        elif score > 80:
-            student_grade[student] = "Excellent"
-        elif score > 70:
-            student_grade[student] = "Very good"
-        else:
-            student_grade[student] = "Failed class"
-    print(student_grade)
-
-
-print_grade(score_list=student_scores)
+# Accessing a nested dictionary and list
+print(travel_log["Sweden"]["cities_visited"][1])
